00cd0bb271ac3ddda4a12028137699f665300720
Britt Shook <brittanysshook@gmail.com>
2023-12-30 21:24:32 -0500

    work page styling

--- src/pages/work/Index.tsx
+++ src/pages/work/Index.tsx
@@ -3,6 +3,7 @@ import { Card } from "../../components/Card";
 import { Filter } from "../../components/Filter";
 import { WorkData } from "./WorkData";
 import { useState, useEffect } from "react";
+import "./Index.css";
 
 export const Work = () => {
   const [displayCards, setDisplayCards] = useState<React.ReactNode[]>([]);
@@ -45,7 +46,7 @@ export const Work = () => {
   }, []);
 
   return (
-    <main id="home">
+    <main id="work">
       <NavBar type="main" />
       <div className="page-content">
         <h1>Recent Projects</h1>
@@ -53,64 +54,8 @@ export const Work = () => {
           filterCategories={["software engineering", "UX design"]}
           onSelectCategory={handleSelectCategory}
         />
-        <section className="project-cards">
-          {displayCards.map((card) => card)}
-        </section>
+        <section id="project-cards">{displayCards.map((card) => card)}</section>
       </div>
     </main>
   );
 };
-
-{
-  /* <Card
-showDemoButton={true}
-demoLink="test"
-showGithubButton={true}
-githubRepoLink="test"
-title="RESTful API for my hyper-local air quality data"
-description="Raspberry Pi, Python, Django, PostgreSQL"
-imageSrc="images/test.webp"
-altText="test"
-categories={["software"]}
-/>
-<Card
-showCaseStudyButton={true}
-caseStudyPath="/work/mahalia-xiaoqi"
-showGithubButton={true}
-githubRepoLink="https://github.com/brittshook/mahalia"
-title="Driving conversions in a bite-size tattoo portfolio with booking form"
-description="HTML, CSS, JavaScript"
-imageSrc="images/test.webp"
-altText="test"
-categories={["software", "design"]}
-/>
-<Card
-showDemoButton={true}
-demoLink="test"
-showGithubButton={true}
-githubRepoLink="https://github.com/brittshook/pride-pac"
-title="Introducing Pride Pac: Queer-themed take on Pac-Man"
-description="HTML, JavaScript"
-imageSrc="images/test.webp"
-altText="test"
-categories={["software"]}
-/>
-<Card
-showCaseStudyButton={true}
-caseStudyPath="/work/state-medicaid-agency"
-title="Leading content design for an enhanced provider experience"
-description="State Medicaid Agency (@Deloitte Consulting)"
-imageSrc="images/test.webp"
-altText="test"
-categories={["design"]}
-/>
-<Card
-showCaseStudyButton={true}
-caseStudyPath="/work/stravafit"
-title="TBD"
-description="TypeScript, React"
-imageSrc="images/test.webp"
-altText="test"
-categories={["design", "software"]}
-/> */
-}
