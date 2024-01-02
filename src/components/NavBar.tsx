aabde99e98343bc4899d1e9d5f080ea414ae4c69
Britt Shook <brittanysshook@gmail.com>
2024-01-02 08:16:13 -0500

    trigger active secctions on cs pages

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useRef, useState } from "react";
 import { Button } from "./Button";
 import "./NavBar.css";
 import { Link, useLocation } from "react-router-dom";
@@ -5,7 +6,7 @@ import { Link, useLocation } from "react-router-dom";
 type Props = {
   type: "main" | "sub";
   items?: [string, string][];
-}
+};
 
 export const NavBar = ({ type, items }: Props) => {
   const navItems = items || [
@@ -15,11 +16,71 @@ export const NavBar = ({ type, items }: Props) => {
     ["Resume", "/resume"],
     ["Contact", "/contact"],
   ];
+  const [active, setActive] = useState<string | null>(null);
   const location = useLocation();
+  useEffect(() => {
+    if (type === "main") {
+      navItems.forEach(([_, path]) => {
+        if (location.pathname === path) {
+          setActive(path);
+        }
+      });
+    } else if (type === "sub") {
+      const observer = new IntersectionObserver(
+        (entries) => {
+          const vh = window.innerHeight;
+          const viewportMiddle = vh / 2;
+
+          let maxCrossedId = "";
+          let maxCrossedTop = -1;
+          let maxCrossedBottom = -1;
+
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              console.log(entry);
+              const sectionTop = entry.boundingClientRect.top;
+              const sectionBottom = entry.boundingClientRect.bottom;
+
+              if (sectionTop < viewportMiddle && sectionTop > maxCrossedTop) {
+                maxCrossedTop = sectionTop;
+                maxCrossedId = entry.target.id;
+              }
+
+              if (
+                sectionBottom > viewportMiddle &&
+                sectionBottom > maxCrossedBottom
+              ) {
+                maxCrossedBottom = sectionBottom;
+                maxCrossedId = entry.target.id;
+              }
+            }
+          });
+
+          maxCrossedId && setActive("#" + maxCrossedId);
+        },
+        {
+          root: null,
+          rootMargin: "-50% 0px -25% 0px",
+          threshold: 0,
+        }
+      );
+
+      navItems.forEach(([_, path]) => {
+        const target = document.querySelector(path);
+        if (target) {
+          observer.observe(target);
+        }
+      });
+
+      return () => {
+        observer.disconnect();
+      };
+    }
+  }, [type, navItems, location.pathname]);
 
   return (
     <nav>
-      {type == "sub" && (
+      {type === "sub" && (
         <Button
           type="button"
           imgSrc="/icons/arrow_left.svg"
@@ -30,9 +91,7 @@ export const NavBar = ({ type, items }: Props) => {
       <ul>
         {navItems.map(([item, path]) => (
           <li
-            className={
-              location.pathname == path ? "nav-link active" : "nav-link"
-            }
+            className={`nav-link ${active === path && "active"}`}
             key={item}
           >
             {path.startsWith("/") ? (
