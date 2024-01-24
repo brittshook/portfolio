96518ce73d5ea6493b9cfec561450c44c35a692d
Britt Shook <brittanysshook@gmail.com>
2024-01-23 23:25:11 -0500

    component to link to demo mid-case study

---
+++ src/components/ViewDemo.tsx
@@ -0,0 +1,29 @@
+import { useLocation } from "react-router-dom";
+import { projectData } from "../pages/work/ProjectData";
+import { Button } from "./Button";
+
+type Props = {
+  customLabel?: string;
+};
+
+export const ViewDemo = ({ customLabel }: Props) => {
+  const location = useLocation();
+  const data = projectData.find(
+    (project) =>
+      project.caseStudyPath && project.caseStudyPath === location.pathname
+  );
+
+  return (
+    <div className="border-[#c2baba] border-solid border text-t-secondary-light text-[17px] flex justify-between items-center rounded-lg p-4">
+      <span>{customLabel ? customLabel : "Visit website"}</span>
+      <Button
+        type="button"
+        imgSrc="/icons/arrow_topright.svg"
+        altText="demo"
+        showTooltip={false}
+        goToExternalPage={data!.demoLink}
+        className={{ other: "max-sm:w-full" }}
+      ></Button>
+    </div>
+  );
+};
