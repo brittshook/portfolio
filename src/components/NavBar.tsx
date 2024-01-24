5a33937de7866b04b2917b3081cf012428b1fd36
Britt Shook <brittanysshook@gmail.com>
2024-01-24 00:06:01 -0500

    fix issue loading pdf

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -168,16 +168,30 @@ export const NavBar = ({ isCaseStudy, items, subNav }: Props) => {
         {navItems.map(([item, path], index) => (
           <li key={item}>
             {path.startsWith("/") ? (
-              <Link
-                className={`capitalize ${
-                  activeItem === path
-                    ? "text-t-primary hover:text-t-primary font-bold cursor-default"
-                    : "text-t-secondary-light  hover:text-t-secondary-dark cursor-pointer"
-                } hover:no-underline`}
-                to={path}
-              >
-                {item}
-              </Link>
+              path.startsWith("/pdf") ? (
+                <a
+                  className={`capitalize ${
+                    activeItem === path
+                      ? "text-t-primary hover:text-t-primary font-bold cursor-default"
+                      : "text-t-secondary-light  hover:text-t-secondary-dark cursor-pointer"
+                  } hover:no-underline`}
+                  href={path}
+                  target="_blank"
+                >
+                  {item}
+                </a>
+              ) : (
+                <Link
+                  className={`capitalize ${
+                    activeItem === path
+                      ? "text-t-primary hover:text-t-primary font-bold cursor-default"
+                      : "text-t-secondary-light  hover:text-t-secondary-dark cursor-pointer"
+                  } hover:no-underline`}
+                  to={path}
+                >
+                  {item}
+                </Link>
+              )
             ) : (
               <a
                 className={`${
